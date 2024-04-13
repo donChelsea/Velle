fa9b537690a647955a90aed8b7af8e7ecac6db53
Chelsea Katsidzira <1133280@iheartmedia.com>
2024-04-13 17:32:46 -0400

    created navigation bar

--- app/src/main/java/com/example/velle/MainActivity.kt
+++ app/src/main/java/com/example/velle/MainActivity.kt
@@ -4,40 +4,109 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.velle.ui.theme.VelleTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             VelleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        bottomBar = {
+                            NavigationBar {
+                                items.forEachIndexed { index, item ->
+                                    NavigationBarItem(
+                                        label = { Text(text = item.title) },
+                                        selected = selectedItemIndex == index,
+                                        onClick = {
+                                            selectedItemIndex = index
+                                            // navController.navigate(item.title)
+                                        },
+                                        icon = {
+                                            BadgedBox(
+                                                badge = {
+                                                    if (item.badgeCount != null) {
+                                                        Badge {
+                                                            Text(text = item.badgeCount.toString())
+                                                        }
+                                                    } else if (item.hasNews) {
+                                                        Badge()
+                                                    }
+                                                }
+                                            ) {
+                                                Icon(
+                                                    imageVector = if (index == selectedItemIndex) item.selectedIcon else item.unselectedIcon,
+                                                    contentDescription = item.title)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    ) {}
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null,
+)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VelleTheme {
-        Greeting("Android")
-    }
-}
+val items = listOf(
+    BottomNavigationItem(
+        title = "History",
+        selectedIcon = Icons.Filled.List,
+        unselectedIcon = Icons.Outlined.List,
+        hasNews = false,
+        badgeCount = 3
+    ),
+    BottomNavigationItem(
+        title = "Send",
+        selectedIcon = Icons.Filled.Send,
+        unselectedIcon = Icons.Outlined.Send,
+        hasNews = false
+    ),
+    BottomNavigationItem(
+        title = "Account",
+        selectedIcon = Icons.Filled.AccountCircle,
+        unselectedIcon = Icons.Outlined.AccountCircle,
+        hasNews = true
+    )
+)
